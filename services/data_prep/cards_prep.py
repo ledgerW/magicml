c329496db8f682083d402feaf38765240a605280
Ledger <lwest@mosaicdatascience.com>
2020-12-17 00:09:37 -0500

    feat: initial data_prep service; raw to clean

--- services/data_prep/cards_prep.py
+++ services/data_prep/cards_prep.py
@@ -4,8 +4,11 @@ sys.setrecursionlimit(100000)
 
 import os
 import json
+import pathlib
 import logging
 import boto3
+import numpy as np
+import pandas as pd
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
@@ -19,6 +22,7 @@ from libs.response_lib import success, failure
 MNT_PATH = os.getenv('EFS_MOUNT_PATH')
 RAW_BUCKET = os.getenv['RAW_BUCKET']
 CLEAN_BUCKET = os.getenv['CLEAN_BUCKET']
+LOCAL_RAW_PATH = '{}/raw'.format(MNT_PATH)
 LOCAL_CLEAN_PATH = '{}/clean'.format(MNT_PATH)
 
 s3 = boto3.client('s3')
@@ -27,21 +31,43 @@ s3 = boto3.client('s3')
 def worker(event, context):
     '''
     '''
-    print(event)
-
     os.makedirs(LOCAL_CLEAN_PATH, exist_ok=True)
+    os.makedirs(LOCAL_RAW_PATH, exist_ok=True)
 
-    body = json.loads(event['body'])
+    res = s3.list_objects_v2(
+        Bucket=RAW_BUCKET,
+        Prefix='mtgjson'
+    )
+
+    card_files = [file['Key'] for file in res['Contents'] if '/decks/' not in file['Key']]
 
     # Get raw data from S3
-    for file_key in card_files:
-      print(file_key)
-      s3.download_file(RAW_BUCKET, file_key, MNT_PATH + '/')
+    for s3_key in card_files:
+      local_path = LOCAL_RAW_PATH + '/' + file_key.split('/')[-1]
+      s3.download_file(RAW_BUCKET, s3_key, local_path)
 
     # Clean data
-    # DO THAT HERE
+    cards_df = pd.read_csv(LOCAL_RAW_PATH + '/cards.csv')\
+      .drop(columns=['index'])
 
+    sets_df = pd.read_csv(LOCAL_RAW_PATH + '/sets.csv')[['code','name']]\
+      .rename(columns={'name': 'setName', 'code':'setCode'})
+
+    cards_df = cards_df\
+      .merge(sets_df, how='left', on='setCode')
+
+    legs_df = pd.read_csv(LOCAL_RAW_PATH + '/legalities.csv')\
+      .pivot(index='uuid', columns='format', values='status')\
+      .reset_index()\
+      .fillna('Blank')
+
+    cards_df = cards_df\
+      .merge(legs_df, how='left', on='uuid')
+
+    cards_df.to_csv(LOCAL_CLEAN_PATH + '/cards.csv', index=False)
+    
     # Send clean data to S3
-    s3.upload_file(local_path, bucket, file_key)
+    for local_path in pathlib.Path(LOCAL_CLEAN_PATH).glob('*'):
+      s3.upload_file(str(local_path), CLEAN_BUCKET, 'cards/{}'.format(local_path.name))
 
     return success({'status': True})
